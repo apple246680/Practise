f1ab7bb7a1c8e78fe45e69bd7f0fbc83783c97c3
apple246680 <pengy4875@gmail.com>
2023-09-04 22:58:51 +0800

    今天完成這些

---
+++ 103-p2-2/103-p2-2/Program.cs
@@ -0,0 +1,30 @@
+﻿try
+{
+    List<string> setdata = new List<string>();
+    Console.WriteLine("please input 1,2,3:");
+    switch (Console.ReadLine())
+    {
+        case "1":
+            setdata = File.ReadAllLines(@".\in1.txt").ToList();
+            break;
+        case "2":
+            setdata = File.ReadLines(@".\in2.txt").ToList();
+            break;
+        case "3":
+            setdata = File.ReadAllLines(@".\in3.txt").ToList();
+            break;
+    }
+    if (setdata.Count != 0)
+    {
+        var a1 = setdata[0].Split(' ').ToList();
+        var a2 = setdata[1].Split(' ').ToList();
+        Console.WriteLine($"count:{a1.Intersect(a2).Distinct().Count()}");
+    }
+    else
+        Console.WriteLine("error");
+    Console.ReadKey();
+}
+catch (Exception)
+{
+    Console.WriteLine("error");
+}
---
+++ 104-p3-1/104-p3-1/Program.cs
@@ -0,0 +1,33 @@
+﻿try
+{
+    List<string> inputFilePaths = new List<string>() { @".\in1.txt", @".\in2.txt" };
+    string outputData = "";
+    foreach (var filePath in inputFilePaths)
+    {
+        var linesFromFile = File.ReadAllLines(filePath);
+        if (1 <= int.Parse(linesFromFile[0]) && int.Parse(linesFromFile[0]) <= 5)
+        {
+            foreach (var line in linesFromFile.Skip(1))
+            {
+                int parsedInt = Convert.ToInt32(line);
+                if (0 <= parsedInt && parsedInt <= 65535)
+                {
+                    int bitCount = 0;
+                    string binaryRepresentation = Convert.ToString(parsedInt, 2);
+                    foreach (var per in binaryRepresentation)
+                        if (per == '1')
+                            bitCount++;
+                    outputData += bitCount + Environment.NewLine;
+                }
+            }
+            outputData += Environment.NewLine;
+        }
+    }
+    File.WriteAllText(@".\out.txt", outputData);
+    Console.WriteLine("OK");
+    Console.ReadKey();
+}
+catch
+{
+    Console.WriteLine("error");
+}
