f599aa8a414e7e54a63b762c7220b778f67f77c5
apple246680 <pengy4875@gmail.com>
2023-09-06 07:24:37 +0800

    抱歉忘記上傳

---
+++ 101-p1-1/101-p1-1/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] filesname = { @".\in1.txt", @".\in2.txt" };
+using (StreamWriter writer = new StreamWriter("output.txt"))
+{
+    foreach (string file in filesname)
+    {
+        var lines = File.ReadAllLines(file).ToList();
+        var wordCount = Regex.Matches(File.ReadAllText(file).ToLower(), $@"\b{lines[0].ToLower()}\b", RegexOptions.IgnoreCase).Count;
+        var totalWordCount = 0;
+        for (int i = 1; i < lines.Count - 1; i++)
+        {
+            string line = lines[i];
+            string[] words = line.Split(new char[] { ' ', ',', ';', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            totalWordCount += words.Length;
+        }
+        writer.WriteLine($"{wordCount - 1},{totalWordCount}" + Environment.NewLine);
+    }
+}
+Console.WriteLine("OK");
+Console.ReadKey();
