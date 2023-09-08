aa1a6013ae910fcd6d0949b97257e242715c0aa2
apple246680 <pengy4875@gmail.com>
2023-09-08 22:35:22 +0800

    這是今天做的進度

---
+++ 101-p4-1/101-p4-1/Program.cs
@@ -0,0 +1,51 @@
+﻿try
+{
+    string outstring = "";
+    foreach (var path in new List<string> { @".\in1.txt", @"in2.txt" })
+    {
+        foreach (var line in File.ReadAllLines(path).Skip(1))
+            outstring += $"{S10TO16(S2TO10(line))},{S10TO8(S2TO10(line))}{Environment.NewLine}";
+        outstring += Environment.NewLine;
+    }
+    File.WriteAllText(@".\out.txt", outstring);
+    Console.WriteLine("OK");
+    Console.ReadKey();
+}
+catch
+{
+    Console.WriteLine("error");
+}
+static int S2TO10(string Number)
+{
+    int decimalNumber = 0;
+    int binaryLength = Number.Length;
+    for (int i = 0; i < binaryLength; i++)
+        decimalNumber += (Number[binaryLength - i - 1] - '0') * (int)Math.Pow(2, i);
+    return decimalNumber;
+}
+static string S10TO8(int Number)
+{
+    if (Number == 0)
+        return "0";
+    string octalNumber = string.Empty;
+    while (Number > 0)
+    {
+        octalNumber = (Number % 8).ToString() + octalNumber;
+        Number /= 8;
+    }
+    return octalNumber;
+}
+static string S10TO16(int Number)
+{
+    if (Number == 0)
+        return "0";
+    string hexadecimalNumber = string.Empty;
+    while (Number > 0)
+    {
+        int remainder = Number % 16;
+        char hexDigit = (remainder < 10) ? (char)(remainder + '0') : (char)(remainder - 10 + 'A');
+        hexadecimalNumber = hexDigit + hexadecimalNumber;
+        Number /= 16;
+    }
+    return hexadecimalNumber;
+}
---
+++ 103-3-1/103-3-1/Program.cs
@@ -0,0 +1,102 @@
+﻿try
+{
+    string outstring = "";
+    foreach (var filePath in new List<string> { @".\in1.txt", @".\in2.txt" })
+    {
+        foreach (var line in File.ReadAllLines(filePath).Skip(1).ToList())
+            outstring += (CalculateMaxScore(line.Split(' ').Select(int.Parse).OrderBy(card => card).Select(card => FormatCard(card)).ToList())) + Environment.NewLine;
+        outstring += Environment.NewLine;
+    }
+    File.WriteAllText(@".\out.txt", outstring);
+    Console.WriteLine("OK");
+    Console.ReadKey();
+}
+catch
+{
+    Console.WriteLine("error");
+}
+static string FormatCard(int card)
+{
+    var suits = new List<string> { "黑桃", "紅桃", "方塊", "梅花" };
+    var values = Enumerable.Range(1, 13).Select(value => value.ToString()).ToList();
+    return suits[(card - 1) / 13] + " " + values[(card - 1) % 13];
+}
+static bool IsStraight(List<string> hand)
+{
+    var cardValues = hand.Select(card => card.Split(' ')[1]).OrderBy(value => value).ToList();
+    if (cardValues.SequenceEqual(new List<string> { "1", "10", "11", "12", "13" }) || cardValues.SequenceEqual(new List<string> { "2", "3", "4", "5", "1" }))
+        return true;
+    for (int i = 1; i < cardValues.Count; i++)
+        if (int.Parse(cardValues[i]) != int.Parse(cardValues[i - 1]) + 1)
+            return false;
+    return true;
+}
+static int CalculateScore(List<string> hand)
+{
+    var cardCounts = new Dictionary<string, int>();
+    foreach (var card in hand)
+    {
+        var cardValue = card.Split(' ')[1];
+        if (cardCounts.ContainsKey(cardValue))
+            cardCounts[cardValue]++;
+        else
+            cardCounts[cardValue] = 1;
+    }
+    var countValues = cardCounts.Values.OrderBy(value => value).ToList();
+    if (IsStraightFlush(hand))
+        return 7;
+    else if (countValues.SequenceEqual(new List<int> { 1, 4 }))
+        return 6;
+    else if (countValues.SequenceEqual(new List<int> { 2, 3 }))
+        return 5;
+    else if (IsStraight(hand))
+        return 4;
+    else if (countValues.SequenceEqual(new List<int> { 1, 1, 3 }))
+        return 3;
+    else if (countValues.SequenceEqual(new List<int> { 1, 2, 2 }))
+        return 2;
+    else if (countValues.SequenceEqual(new List<int> { 1, 1, 1, 2 }))
+        return 1;
+    else
+        return 0;
+}
+static bool IsStraightFlush(List<string> hand)
+{
+    var cardValues = hand.Select(card => card.Split(' ')[1]).OrderBy(value => value).ToArray();
+    for (int i = 1; i < cardValues.Length; i++)
+        if (int.Parse(cardValues[i]) != int.Parse(cardValues[i - 1]) + 1)
+            return false;
+    return true;
+}
+static List<List<string>> GenerateCombinations(List<string> hand)
+{
+    var combinations = new List<List<string>>();
+    void Generate(int start, List<string> currentCombination)
+    {
+        if (currentCombination.Count == 5)
+        {
+            combinations.Add(new List<string>(currentCombination));
+            return;
+        }
+        for (int i = start; i < hand.Count; i++)
+        {
+            currentCombination.Add(hand[i]);
+            Generate(i + 1, currentCombination);
+            currentCombination.RemoveAt(currentCombination.Count - 1);
+        }
+    }
+    Generate(0, new List<string>());
+    return combinations;
+}
+static int CalculateMaxScore(List<string> hand)
+{
+    var maxScore = 0;
+    var combinations = GenerateCombinations(hand);
+    foreach (var combination in combinations)
+    {
+        var score = CalculateScore(combination);
+        if (score > maxScore)
+            maxScore = score;
+    }
+    return maxScore;
+}
