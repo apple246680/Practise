4bbeba7d8d70cdacff3ca5ffedba21c408a5134f
apple246680 <pengy4875@gmail.com>
2023-09-09 23:09:25 +0800

    這是今天的進度

---
+++ 104-3/104-3/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace _104_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ShowPictureBox = new PictureBox();
+            OpenImageBtn = new Button();
+            ClearBtn = new Button();
+            openFileDialog = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)ShowPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // ShowPictureBox
+            // 
+            ShowPictureBox.Location = new Point(12, 12);
+            ShowPictureBox.Name = "ShowPictureBox";
+            ShowPictureBox.Size = new Size(776, 272);
+            ShowPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            ShowPictureBox.TabIndex = 0;
+            ShowPictureBox.TabStop = false;
+            // 
+            // OpenImageBtn
+            // 
+            OpenImageBtn.Location = new Point(169, 340);
+            OpenImageBtn.Name = "OpenImageBtn";
+            OpenImageBtn.Size = new Size(94, 29);
+            OpenImageBtn.TabIndex = 1;
+            OpenImageBtn.Text = "開啟圖檔...";
+            OpenImageBtn.UseVisualStyleBackColor = true;
+            OpenImageBtn.Click += OpenImageBtn_Click;
+            // 
+            // ClearBtn
+            // 
+            ClearBtn.Enabled = false;
+            ClearBtn.Location = new Point(500, 340);
+            ClearBtn.Name = "ClearBtn";
+            ClearBtn.Size = new Size(94, 29);
+            ClearBtn.TabIndex = 2;
+            ClearBtn.Text = "侵蝕處理";
+            ClearBtn.UseVisualStyleBackColor = true;
+            ClearBtn.Click += ClearBtn_Click;
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = ".bmp";
+            openFileDialog.Filter = "BMP檔案(*.bmp)|*.bmp";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ClearBtn);
+            Controls.Add(OpenImageBtn);
+            Controls.Add(ShowPictureBox);
+            Name = "Form1";
+            Text = " 侵蝕處理";
+            ((System.ComponentModel.ISupportInitialize)ShowPictureBox).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox ShowPictureBox;
+        private Button OpenImageBtn;
+        private Button ClearBtn;
+        private OpenFileDialog openFileDialog;
+    }
+}
---
+++ 104-3/104-3/Form1.cs
@@ -0,0 +1,72 @@
+﻿namespace _104_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string imagePath = "";
+        public bool[] GetSurroundingPixels(Bitmap image, int x, int y)
+        {
+            bool[] surroundingPixels = new bool[8];
+            Color pixelColor;
+            int pixelIndex = 0;
+            for (int xOffset = -1; xOffset <= 1; xOffset++)
+            {
+                for (int yOffset = -1; yOffset <= 1; yOffset++)
+                {
+                    pixelColor = image.GetPixel(x + xOffset, y + yOffset);
+                    if (xOffset != 0 || yOffset != 0)
+                    {
+                        if (pixelColor.G == 255)
+                        {
+                            surroundingPixels[pixelIndex] = false;
+                            pixelIndex++;
+                        }
+                        else if (pixelColor.G == 0)
+                        {
+                            surroundingPixels[pixelIndex] = true;
+                            pixelIndex++;
+                        }
+                    }
+                }
+            }
+            return surroundingPixels;
+        }
+        private void OpenImageBtn_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                imagePath = openFileDialog.FileName;
+                ShowPictureBox.ImageLocation = imagePath;
+                ClearBtn.Enabled = true;
+            }
+        }
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap originalImage = new Bitmap(ShowPictureBox.Image);
+            Bitmap corrodedImage = new Bitmap(ShowPictureBox.Image);
+            bool[] surroundingPixels;
+            for (int x = 1; x < ShowPictureBox.Image.Width - 1; x++)
+            {
+                for (int y = 1; y < ShowPictureBox.Image.Height - 1; y++)
+                {
+                    if (originalImage.GetPixel(x, y).R == 0)
+                    {
+                        surroundingPixels = GetSurroundingPixels(originalImage, x, y);
+                        for (int index = 0; index < surroundingPixels.Length; index++)
+                        {
+                            if (!surroundingPixels[index])
+                            {
+                                corrodedImage.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            ShowPictureBox.Image = corrodedImage;
+        }
+    }
+}
