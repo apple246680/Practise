db9d3aa05301080610a72770a38ae8254df21034
apple246680 <pengy4875@gmail.com>
2023-09-17 12:56:04 +0800

    1

--- 商科賽/second/99 p1-1/99 p1-1/Program.cs
+++ 商科賽/second/99 p1-1/99 p1-1/Program.cs
@@ -4,7 +4,11 @@
     foreach (var path in new List<string>() { @".\in1.txt", @".\in2.txt" })
     {
         var openfile = File.ReadAllText(path).Replace("，", "").Replace("、", "").Replace("。", "").Replace("\r\n", "").ToCharArray();
-        var alltext = openfile.GroupBy(x => x).OrderByDescending(X => X.Count()).Select(x => new { key = x.Key, value = x.Count() });
+        var alltext = openfile
+            .GroupBy(x => x)
+            .OrderByDescending(X => X.Count())
+            .Select(x => new { key = x.Key, value = x.Count() });
+
         var max = alltext.Max(x => x.value);
         alltext.Where(x => x.value == max).ToList().ForEach(x =>
         {
