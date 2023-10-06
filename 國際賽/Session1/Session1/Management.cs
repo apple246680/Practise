a329cda7b5b1d3d59cede1718e9afa716ce30bb4
apple246680 <pengy4875@gmail.com>
2023-10-06 20:03:13 +0800

    a

--- 國際賽/Session1/Session1/Management.cs
+++ 國際賽/Session1/Session1/Management.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 namespace Session1
 {
     public partial class Management : Form
