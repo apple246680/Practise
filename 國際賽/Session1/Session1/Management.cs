06aa1775db57e37f1a0174c80d6e77b53e3ce465
apple246680 <pengy4875@gmail.com>
2023-10-07 22:53:43 +0800

    a

--- 國際賽/Session1/Session1/Management.cs
+++ 國際賽/Session1/Session1/Management.cs
@@ -18,70 +18,94 @@ namespace Session1
         {
             InitializeComponent();
         }
-        List<int>datacount = new List<int>() { 0,0};
+        bool IsClose;
         private void LogoutBtn_Click(object sender, EventArgs e)
         {
             Global.accountID = null;
             Properties.Settings.Default.KeepLogin = "";
             Properties.Settings.Default.Save();
+            IsClose = false;
             Global.login.Visible = true;
             MessageBox.Show("Success!");
             Close();
         }
         private void ExitBtn_Click(object sender, EventArgs e)
         {
+            IsClose = true;
             Close();
         }
+
         private void hint_Click(object sender, EventArgs e)
         {
             hint.Visible = false;
-            search.Focus();
         }
+        public List<int> CurrentNum = new List<int>() { 0, 0 };
         private void search_TextChanged(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(search.Text))
-                return;
-            var entities = new Session1Entities();
-            var data = entities.Items.Where(x => x.Title == search.Text || x.Area.Name == search.Text || x.ItemAttractions.Any(y => y.Distance <= 1 && y.Attraction.Name == search.Text)).Select(x => new
+            TravelerDataGridView.Rows.Clear();
+            new Session1Entities().Items.Where(x => x.Title == search.Text || x.Area.Name == search.Text || x.ItemAttractions.Any(y => y.Distance <= 1 && y.Attraction.Name == search.Text)).ToList().ForEach(x =>
             {
-                Title=x.Title,
-                Capacity=x.Capacity,
-                Area=x.Area.Name,
-                Type=x.ItemType.Name,
-            }).ToList();
-            TravelerDataGridView.DataSource = data;
-            datacount[tabControl1.SelectedIndex] = data.Count;
-            CountLabel.Text = $"{data.Count} items found.";
+                TravelerDataGridView.Rows.Add(x.Title, x.Capacity, x.Area.Name, x.ItemType.Name);
+            });
+            CurrentNum[0] = TravelerDataGridView.Rows.Count;
+            CountLabel.Text = $"{TravelerDataGridView.Rows.Count} items found.";
         }
         private void search_Leave(object sender, EventArgs e)
         {
-            if(string.IsNullOrWhiteSpace(search.Text))
+            if (String.IsNullOrEmpty(search.Text))
                 hint.Visible = true;
         }
         private void search_Enter(object sender, EventArgs e)
         {
-            
-
+            hint.Visible = false;
         }
         private void Management_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Global.login.Close();
+            if (IsClose)
+            {
+                Global.login.Close();
+                Application.Exit();
+            }
+            else
+                Global.login.Visible = true;
         }
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            CountLabel.Text = $"{CurrentNum[tabControl1.SelectedIndex]} items found.";
         }
         private void Management_Load(object sender, EventArgs e)
         {
-            
+            ManageDataGridView.Rows.Clear();
+            Session1Entities entities = new Session1Entities();
+            entities.Items.Where(t => t.UserID == Global.accountID).ToList().ForEach(t =>
+            {
+                ManageDataGridView.Rows.Add(t.ID, t.Title, t.Capacity, t.Area.Name, t.ItemType.Name, "Edit Details");
+            });
+            CurrentNum[1] = ManageDataGridView.Rows.Count;
         }
         private void AddListingBtn_Click(object sender, EventArgs e)
         {
-            
+            AddOrEditListinig addOrEditListinig = new AddOrEditListinig(null);
+            addOrEditListinig.ShowDialog();
+            Management_Load(sender, e);
         }
+
         private void ManageDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            if (e.RowIndex < 0 || e.ColumnIndex != 5)
+                return;
+            long id = (long)ManageDataGridView.Rows[e.RowIndex].Cells[0].Value;
+            AddOrEditListinig form = new AddOrEditListinig(id);
+            form.ShowDialog();
+            ManageDataGridView.Rows.Clear();
+            Session1Entities entities = new Session1Entities();
+            var data = entities.Items.Where(x => x.UserID == Global.accountID).ToList();
+            data.ForEach(x =>
+            {
+                ManageDataGridView.Rows.Add(x.ID, x.Title, x.Capacity, x.Area.Name, x.ItemType.Name, "Edit Details");
+            });
+            CurrentNum[1] = data.Count();
+            CountLabel.Text = $"{data.Count()} items found.";
         }
     }
 }
