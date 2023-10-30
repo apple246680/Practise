7aba2f369c15449df80ab29ff694dbaa4117715e
apple246680 <pengy4875@gmail.com>
2023-10-30 23:23:56 +0800

    a

--- 國際賽/練速度first/Session3/Session3UnitTestProject/Session3UnitTest.cs
+++ 國際賽/練速度first/Session3/Session3UnitTestProject/Session3UnitTest.cs
@@ -1,16 +1,65 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Session3;
+using System.Linq;
 namespace Session3UnitTestProject
 {
     [TestClass]
     public class Session3UnitTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [DataRow("dadada", "dadada", "dadadad", true, 1)]
+        [DataRow("aagag", "aagag", "aagag", true, 1)]
+        [DataRow("mahdi", "1234", "Min-Seo Young-Ho", true, 1)]
+        public void TestUserRegister(string username, string password, string fullname, bool gender, int familyCount)
         {
-            var entities=new Session3Entities();
-            
+            var birthday = DateTime.Today;
+            User user;
+            try
+            {
+                user = Global.Register(username, fullname, password, birthday, familyCount, gender);
+                using (var entities = new Session3Entities())
+                {
+                    user = entities.Users.SingleOrDefault(t => t.ID == user.ID);
+                    Assert.IsTrue(user != null, "Register user is not found.");
+                    Assert.IsTrue(user.Username == username, "Register user Username value different.");
+                    Assert.IsTrue(user.Password == password, "Register Password value different.");
+                    Assert.IsTrue(user.FullName == fullname, "Register FullName value different.");
+                    Assert.IsTrue(user.Gender == gender, "Register Gender value different.");
+                    Assert.IsTrue(user.BirthDate == birthday, "Register BirthDate value different.");
+                    Assert.IsTrue(user.FamilyCount == familyCount, "Register FamilyCount value different.");
+                    entities.Users.Remove(entities.Users.SingleOrDefault(t => t.ID == user.ID));
+                    entities.SaveChanges();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+        [TestMethod]
+        public void TestItemScore()
+        {
+            try
+            {
+                Global.CheckScore();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+        [TestMethod]
+        public void TestItemAvailableDate()
+        {
+            try
+            {
+                Global.CheckItemDate();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
         }
     }
 }
