58047db424f9048a7d76838e6e90b1af8f6a9f39
apple246680 <pengy4875@gmail.com>
2023-10-30 12:49:02 +0800

    a

--- 國際賽/練速度first/Session3/Session3UnitTestProject/UnitTest1.cs
+++ 國際賽/練速度first/Session3/Session3UnitTestProject/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-
+using Session3;
 namespace Session3UnitTestProject
 {
     [TestClass]
@@ -9,6 +9,8 @@ namespace Session3UnitTestProject
         [TestMethod]
         public void TestMethod1()
         {
+            var entities=new Session3Entities();
+
         }
     }
 }
