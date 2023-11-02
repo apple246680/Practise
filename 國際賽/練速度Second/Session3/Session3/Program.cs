51e855c9ce2467f841be157318e3dc497b428d88
apple246680 <pengy4875@gmail.com>
2023-11-02 23:57:07 +0800

    a

---
+++ 國際賽/練速度Second/Session3/Session3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Session3
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += HandleUnhandledException;
+            Application.Run(new MainForm());
+        }
+        private static void HandleUnhandledException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show($"error: {e.Exception.Message}", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
