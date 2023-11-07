73be038d73af7aecb8913dcbbd9bc4284bea300e
apple246680 <pengy4875@gmail.com>
2023-11-07 23:14:10 +0800

    a

---
+++ 國際賽/練速度Third/Session1/Session1/CreateAccountForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session1
+{
+    public partial class CreateAccountForm : Form
+    {
+        /// <summary>
+        /// InitializeComponent CreateAccountForm
+        /// </summary>
+        public CreateAccountForm()
+        {
+            InitializeComponent();
+        }
+        int close = 1;
+        /// <summary>
+        /// Button style
+        /// </summary>
+        private void button_Down(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).ForeColor = Color.FromArgb(229, 26, 46);
+        }
+        /// <summary>
+        /// Button style
+        /// </summary>
+        private void button_Up(object sender, MouseEventArgs e)
+        {
+            ((Button)sender).ForeColor = Color.FromArgb(85, 85, 85);
+        }
+        /// <summary>
+        /// Show TermsForm
+        /// </summary>
+        private void TermsLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Visible = false;
+            new TermsForm().ShowDialog();
+            TermsCheckBox.Enabled = true;
+            Visible = true;
+        }
+        /// <summary>
+        /// Closing CreateAccountForm
+        /// </summary>
+        private void CreateAccountForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (close)
+            {
+                case 1:
+                    Application.Exit();
+                    break;
+                case 2:
+                    Global.LoginForm.Visible = true;
+                    break;
+                case 3:
+                    break;
+            }
+        }
+        /// <summary>
+        /// Return to LoginForm
+        /// </summary>
+        private void ReturnLoginFormBtn_Click(object sender, EventArgs e)
+        {
+            close = 2;
+            CreateAccountForm_FormClosing(null,null);
+        }
+        /// <summary>
+        /// Register the Application and Login
+        /// </summary>
+        private void RegisterAndLoginBtn_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(UsernameTextBox.Text) || String.IsNullOrWhiteSpace(PasswordTextBox.Text) || String.IsNullOrWhiteSpace(RetypePasswordTextbox.Text) || String.IsNullOrWhiteSpace(FullNameTextBox.Text))
+            {
+                MessageBox.Show("Input can't be empty.");
+                return;
+            }
+            if (PasswordTextBox.Text != RetypePasswordTextbox.Text)
+            {
+                MessageBox.Show("Password and ReType-Password is not equals.");
+                return;
+            }
+            if (!TermsCheckBox.Checked)
+            {
+                MessageBox.Show("Please View Terms Document And Click Agree Conditions First.");
+                return;
+            }
+            Global.accountID = Global.Register(UsernameTextBox.Text, FullNameTextBox.Text, PasswordTextBox.Text, BirthdayDateTimePicker.Value, (int)FamilyNumericUpDown.Value, MaleRadioButton.Checked).ID;
+            new ManagementForm().Show();
+            close = 2;
+            Close();
+            MessageBox.Show("OK");
+        }
+    }
+}
---
+++ 國際賽/練速度Third/Session1/Session1/TermsForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Session1
+{
+    partial class TermsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TermsForm));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(187)))), ((int)(((byte)(187)))));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(800, 660);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // TermsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(187)))), ((int)(((byte)(187)))));
+            this.ClientSize = new System.Drawing.Size(800, 660);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Open Sans Light", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "TermsForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seoul Stay - Terms";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
